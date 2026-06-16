7e78b77ad9d55a76bdb233a7c74fc5b0da4444c5
nick1842 <139996617+nick1842@users.noreply.github.com>
2026-06-16 11:05:00 -0500

    sad

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 
 function cleanUrl(url) {
