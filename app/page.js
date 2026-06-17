22a2b4db62d23a06765664cd1c755e93619f2a83
nick1842 <139996617+nick1842@users.noreply.github.com>
2026-06-16 19:26:27 -0500

    sad

--- app/page.js
+++ app/page.js
@@ -110,6 +110,12 @@ export default function Home() {
   (n) => n.read === false
 ).length;
   const [hasUnreadMessages, setHasUnreadMessages] = useState(false);
+
+  useEffect(() => {
+    loadEverything();
+    checkUnreadMessages();
+  }, []);
+
   useEffect(() => {
   function updateDatingTime() {
     const startDate = new Date("2024-09-21T00:00:00");
